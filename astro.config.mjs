78104fa260de1db91e728cde5ec5e33bc0ec13a8
citrus-rol <c.citrus.rol@gmail.com>
2025-08-15 03:07:34 +0800

    更新網站內容與元件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  base: '/my-astro-site/',  // 根路徑，對應 GitHub Pages 子路徑
+  base: '/carol-lab/',  // 根路徑，對應 GitHub Pages 子路徑
   outDir: 'docs',           // 輸出資料夾
 });
